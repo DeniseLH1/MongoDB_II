a71eaa04a85530932139a03689713261f71a6361
DeniseLH1 <jdlhprincesa@gmial.com>
2026-07-08 11:48:13 -0600

    feat: nuevas luneas de codigo

--- Secion 7/creacion_ususario.js
+++ Secion 7/creacion_ususario.js
@@ -1,6 +1,7 @@
 db.createUser({
     user:'auxiliarventas',
     pws: 'Abcd123*',
+    customData:{description:'Usuario auxiliar de ventas encargado de los repostes',userId:'asdfa41as3fas3df1a1df'},
     roles:[
         'read',
         {role:'readWrite',db:'campus'},
