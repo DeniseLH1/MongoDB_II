c94cc438959f93876d5f336e6074fc0fc4a18246
TOTO05-a <ac138439@gmail.com>
2026-07-10 11:27:36 -0600

    feat: agregar archivos para ejercicios de la secion 11

---
+++ Skill_MongoDB_Toto_E2/MongoDB2/secion11/gerentes.js
@@ -0,0 +1,14 @@
+[
+  {
+    "_id": ObjectId("65d44444f1d234567890aa01"),
+    "nombre": "Laura Casillas",
+    "telefono": "+525512345678",
+    "salario": 4500.00
+  },
+  {
+    "_id": ObjectId("65d44444f1d234567890aa02"),
+    "nombre": "Roberto Forlan",
+    "telefono": "+573001234567",
+    "salario": 4200.00
+  }
+]
---
+++ Skill_MongoDB_Toto_E2/MongoDB2/secion11/libros.js
@@ -0,0 +1,19 @@
+[
+  {
+    "_id": ObjectId("65e77777f1d234567890dd01"),
+    "titulo": "Cien años de soledad",
+    "paginas": 471,
+    "autores_ids": [
+      ObjectId("65e66666f1d234567890cc01")
+    ]
+  },
+  {
+    "_id": ObjectId("65e77777f1d234567890dd02"),
+    "titulo": "Libro Escrito en Colaboración",
+    "paginas": 210,
+    "autores_ids": [
+      ObjectId("65e66666f1d234567890cc01"),
+      ObjectId("65e66666f1d234567890cc02")
+    ]
+  }
+]
